f1ba2f21f5bc7a3a1e73a96451b01ab04313639b
Vitaliy Kachan <kachan.vitaliy12@gmail.com>
2026-03-17 14:51:36 +0200

    feat: updating angular to 18

--- src/app/shared/NgxMatHighlightDirective.ts
+++ src/app/shared/NgxMatHighlightDirective.ts
@@ -1,6 +1,5 @@
 import { Directive, ElementRef, AfterViewInit } from '@angular/core';
-// import * as hljs from 'highlight.js';
-var hljs = require('highlight.js/lib/common');
+import highlightjs from 'highlight.js/lib/common';
 
 @Directive({
     selector: 'code[ngxMatHighlight]'
@@ -9,6 +8,7 @@ export class NgxMatHighlightDirective implements AfterViewInit {
     constructor(private eltRef: ElementRef) {
     }
     ngAfterViewInit() {
-        (hljs as any).highlightBlock(this.eltRef.nativeElement);
+        const hljs: any = (highlightjs as any).default || highlightjs;
+        hljs.highlightElement(this.eltRef.nativeElement);
     }
 }
